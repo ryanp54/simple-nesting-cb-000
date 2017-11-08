5b3ef8cfd6983823f745459e8b2318b63f242d9a
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-08 17:18:14 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -61,11 +61,6 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -81,6 +76,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
+programmer_hash[:yukihiro_matsumoto] => {
+	:known_for => "Ruby",
+	:languages => ["LISP", "C"]
+}
 
 
 end
