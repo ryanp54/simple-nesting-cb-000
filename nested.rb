1796cefc7cb70f0233ff65d49e98984a54513709
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-11-08 17:22:07 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -80,6 +80,7 @@ def adding_matz
 		:known_for => "Ruby",
 		:languages => ["LISP", "C"]
 	}
+	programmer_hash
 end
 
 def changing_alan
@@ -101,7 +102,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 		 programmer_hash[:alan_kay][:known_for] = alans_new_info
-
+		 programmer_hash
 end
 
 def adding_to_dennis
@@ -121,4 +122,5 @@ def adding_to_dennis
         }
      }
 	programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+	programmer_hash
 end
